71c02a2324795bbe985d26d0bebfe8cd421645fe
Alifi Az Zahra <alifiaazzahra1505@gmail.com>
2026-09-08 09:06:23 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Halo, Alifia!");
+        System.out.print("Nama: ");
+        System.out.print("Pia");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
