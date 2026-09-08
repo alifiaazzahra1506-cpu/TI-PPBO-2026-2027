fc873b4fb7c71138942624c4449410402a07edde
Alifi Az Zahra <alifiaazzahra1505@gmail.com>
2026-09-08 11:11:04 +0700

    Latihan nomor 3

---
+++ praktikum-2/src/DataPribadi.java
@@ -0,0 +1,14 @@
+public class DataPribadi {
+    public static void main(String[] args) {
+
+        int umur = 19;
+        double tinggiBadan = 158;
+        char inisial = 'A';
+        boolean mahasiswaAktif = true;
+
+        System.out.println("Umur: 19 " + " tahun");
+        System.out.println("Tinggi Badan: " + tinggiBadan + " cm");
+        System.out.println("Inisial: " + inisial);
+        System.out.println("Mahasiswa Aktif: " + mahasiswaAktif);
+    }
+}
