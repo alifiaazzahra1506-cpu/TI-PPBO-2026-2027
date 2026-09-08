2004687ca9072c51a0b2715261716433451118f0
Alifi Az Zahra <alifiaazzahra1505@gmail.com>
2026-09-08 10:54:23 +0700

    Latihan nomor 1

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,14 @@
+public class BiodataSaya {
+    public static void main(String[] args) {
+
+
+        //Program untuk menampilkan biodata
+        //Menampilkan biodata berdasarkan Nama, NIM
+
+        System.out.println("Nama: Alifia Az Zahra ");
+        System.out.println("NIM: 2025573010041");
+
+        //Menampilkan Program Studi pada baris terpisah
+        System.out.println("Program Studi: Teknik Informatika");
+    }
+}
